d64a1b75fb542d915bed9dac274fe1b8e04f04d9
IElectricityGood <syedtahmidm@gmail.com>
2022-10-01 01:21:34 -0700

    Fixed path to conf in finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt) # assumes where username.txt is
 
 if [ $# -lt 2 ]
 then
